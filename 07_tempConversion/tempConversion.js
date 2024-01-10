8e23e787af75939d99f89775448bc436d1c38b1f
Karl Perkins <karl@karlperkins.me>
2024-01-11 08:17:55 +1300

    Complete Exercise 07 - tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  // Farenheit to Celsius: x °F ≘ (x − 32) × 5/9 °C
+  const celsius = (farenheit - 32) * 5 / 9;
+
+  // Round to 1 decimal place
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  // Celsius to Farenheit: x °C ≘ (x × 9/5 + 32) °F
+  const farenheit = (celsius * 9 / 5 + 32)
+
+  // Round to 1 decimal place
+  return Math.round(farenheit * 10) / 10;
 };
 
 // Do not edit below this line
