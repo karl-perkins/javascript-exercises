ced81e13ab8f7d8b320d98ad13b3756bcc72aa02
Karl Perkins <karl@karlperkins.me>
2024-01-10 15:14:43 +1300

    Complete Exercise 04 - removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+
+  const newArr = arr;
+
+  for (const arg of args) {
+    
+    const index = newArr.indexOf(arg);
+
+    if (index === -1) continue;
+
+    newArr.splice(index, 1);
+
+  }
+
+  return newArr;
 
 };
 
