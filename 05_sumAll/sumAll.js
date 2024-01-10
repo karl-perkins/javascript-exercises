1ba6db7f40b4a5ac16a81ebad39ec55148324fbf
Karl Perkins <karl@karlperkins.me>
2024-01-10 15:56:26 +1300

    Complete Exercise 05 - sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+	// Confirm that both arguments are positive integers
+	if (
+		num1 < 1 ||
+		typeof num1 != "number" ||
+		num2 < 1 ||
+		typeof num2 != "number"
+	) {
+		return "ERROR";
+	}
 
+	const args = [num1, num2].sort();
+
+	let sum = 0;
+
+	for (let i = args[0]; i <= args[1]; i++) {
+		sum += i;
+	}
+
+	return sum;
 };
 
 // Do not edit below this line
