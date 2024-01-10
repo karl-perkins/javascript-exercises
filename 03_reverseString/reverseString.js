3f2a8d3b389da9024dee4c779fa3af22d1e8154f
Karl Perkins <karl@karlperkins.me>
2024-01-10 14:54:00 +1300

    Complete Exercise 03 - Reverse a String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+  let charArr = string.split('');
+  
+  charArr.reverse(); // Could also use toReversed() and pass return value into a variable
+
+  const reversedString = charArr.join('');
+
+  return reversedString;
 
 };
 
