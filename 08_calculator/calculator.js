26f16a29c36528904ccc10544787921e6ae65a25
Karl Perkins <karl@karlperkins.me>
2024-01-17 15:36:16 +1300

    Complete Exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, cur) => sum + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, cur) => product * cur); // Don't provide an initial value
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) return 1;
+	let product = n;
+  // Factorial formula: n! = n * (n-1) * ... * 1
+  for (let i = 1; i < n; i++) {
+    product *= (n - i);
+  }
+  return product;
 };
 
 // Do not edit below this line
