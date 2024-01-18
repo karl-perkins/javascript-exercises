46f9b95de7f24a3aa6b09b9db504bd22221d6820
Karl Perkins <karl@karlperkins.me>
2024-01-19 08:10:18 +1300

    Complete Exercise 09 - Palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const processed = str.replace(/[\p{P}\s]+/gu, '').toLowerCase();
+  const reversed = processed.split('').reverse().join('');
+  return processed === reversed;
 };
 
 // Do not edit below this line
