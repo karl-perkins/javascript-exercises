01a25e9ac1abda93a13a7f60770754a8135fd539
Karl Perkins <karl@karlperkins.me>
2024-01-20 07:29:40 +1300

    Complete Exercise 10 - Fibonnaci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(memberIndex) {
+  if (memberIndex < 0) return 'OOPS';
+  
+  const seq = [];
+  // Fibonacci sequence: F0 = 0, F1 = 1, and Fn = Fn-1 + Fn-2 for n > 1
+  for (let index = 0; index <= memberIndex; index++) {
+    seq.push(index < 2 ? index : seq[index-1] + seq[index-2]);
+  }
+  return seq[memberIndex];
 };
 
 // Do not edit below this line
