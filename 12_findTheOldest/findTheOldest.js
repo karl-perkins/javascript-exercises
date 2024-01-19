f6bcec7bfca64d92c5252b15161e8ab118429a54
Karl Perkins <karl@karlperkins.me>
2024-01-20 07:47:55 +1300

    Complete Exercise 12 - Find the Oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.sort((personA, personB) => {
+    const currentYear = (new Date).getFullYear(); 
+    const personAAge = (personA.yearOfDeath ?? currentYear) - personA.yearOfBirth;
+    const personBAge = (personB.yearOfDeath ?? currentYear) - personB.yearOfBirth;
+    return personAAge > personBAge ? -1 : 1;
+  })[0];
 };
 
 // Do not edit below this line
